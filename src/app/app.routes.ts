ecdc48463239344178dd15d930b8d7ce25254828
ChineDmitri <mr.dima11@gmail.com>
2024-07-08 10:31:23 +0200

    Gooooo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { MainPageComponent } from './components/main-page/main-page.component';
+import { ProfilComponent } from './components/profil/profil.component';
+import { AddItemComponent } from './components/add-item/add-item.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'page1', pathMatch: 'full' },
+  { path: 'main', component: MainPageComponent },
+  { path: 'profil', component: ProfilComponent },
+  { path: 'item', component: AddItemComponent },
+];
